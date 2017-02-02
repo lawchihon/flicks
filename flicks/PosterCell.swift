826aeedb5294d761f8e19b878a32978f7130adab
John <lawchihon@ymail.com>
2017-02-02 11:55:01 -0800

    Completed CollectionView Display

---
+++ flicks/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  flicks
+//
+//  Created by John Law on 2/2/2017.
+//  Copyright © 2017 Chi Hon Law. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
--- flicks/PosterViewController.swift
+++ flicks/PosterViewController.swift
@@ -1,8 +1,8 @@
 //
-//  MovieViewController.swift
+//  PosterViewController.swift
 //  flicks
 //
-//  Created by John Law on 1/2/2017.
+//  Created by John Law on 2/2/2017.
 //  Copyright © 2017 Chi Hon Law. All rights reserved.
 //
 
@@ -10,26 +10,39 @@ import UIKit
 import AFNetworking
 import MBProgressHUD
 
-class MovieViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    
-    var movies: [NSDictionary]?
+class PosterViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
-    @IBOutlet weak var tableView: UITableView!
+    var movies: [NSDictionary]?
+    @IBOutlet weak var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        collectionView.dataSource = self
+        collectionView.delegate = self
         
-        tableView.dataSource = self
-        tableView.delegate = self
-        
-        // Initialize a UIRefreshControl
         let refreshControl = UIRefreshControl()
         refreshControl.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
-        tableView.insertSubview(refreshControl, at: 0)
-        
-        // Do any additional setup after loading the view.
+        collectionView.insertSubview(refreshControl, at: 0)
+
         loadDataFromNetwork()
     }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
     
     // Makes a network request to get updated data
     // Updates the tableView with the new data
@@ -40,7 +53,7 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
         loadDataFromNetwork()
         refreshControl.endRefreshing()
     }
-    
+
     func loadDataFromNetwork() {
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
@@ -50,14 +63,14 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
         // Showing a progress HUDEdit PagePage History by MBProgressHUD
         // Display HUD right before the request is made
         MBProgressHUD.showAdded(to: self.view, animated: true)
-
+        
         let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
             if let data = data {
                 if let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
                     //print(dataDictionary)
                     
                     self.movies = (dataDictionary["results"] as! [NSDictionary])
-                    self.tableView.reloadData()
+                    self.collectionView.reloadData()
                 }
                 
             }
@@ -66,7 +79,7 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
                     error?.localizedDescription, preferredStyle: UIAlertControllerStyle.alert)
                 alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default,handler: nil))
                 self.present(alertController, animated: true, completion: nil)
-
+                
                 //print (error.debugDescription)
                 //print (error?.localizedDescription as Any)
             }
@@ -76,23 +89,8 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
         }
         task.resume()
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destinationViewController.
-     // Pass the selected object to the new view controller.
-     }
-     */
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         if let movies = movies {
             return movies.count
         }
@@ -101,25 +99,14 @@ class MovieViewController: UIViewController, UITableViewDataSource, UITableViewD
         }
     }
     
-    
-    // Row display. Implementers should *always* try to reuse cells by setting each cell's reuseIdentifier and querying for available reusable cells with dequeueReusableCellWithIdentifier:
-    // Cell gets various attributes set automatically based on table (separators) and data source (accessory views, editing controls)
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
-        
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PosterCell", for: indexPath) as! PosterCell
         let movie = movies![indexPath.row]
-        let title = movie["title"] as! String
-        let overview = movie["overview"] as! String
         let posterPath = movie["poster_path"] as! String
         let baseUrl = "https://image.tmdb.org/t/p/w342"
         let imageUrl = URL(string: baseUrl + posterPath)
-        
-        cell.titleLabel.text = title
-        cell.overviewLabel.text = overview
         cell.posterView.setImageWith(imageUrl!)
         
-        //print ("row \(indexPath.row)")
         return cell
     }
 }
